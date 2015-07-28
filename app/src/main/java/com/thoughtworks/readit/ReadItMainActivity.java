7a68b4c5c1d8ac936164d3d35c73b7c511da0989
jayasagar <jayasagar@gmail.com>
2015-07-28 17:08:32 +0530

    Introduce Volly for network communication calls

--- app/src/main/java/com/thoughtworks/readit/ReadItMainActivity.java
+++ app/src/main/java/com/thoughtworks/readit/ReadItMainActivity.java
@@ -2,6 +2,7 @@ package com.thoughtworks.readit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class ReadItMainActivity extends Activity {
 
@@ -9,5 +10,8 @@ public class ReadItMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+       // (ImageButton)findViewById(R.id.addResourceButton);
     }
 }
---
+++ app/src/main/java/com/thoughtworks/readit/domain/Resource.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.readit.domain;
+
+public class Resource {
+
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
